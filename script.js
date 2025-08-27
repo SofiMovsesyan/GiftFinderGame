5dc439fc23bb0e05fb0cff945080a2458262ec4a
SofiMovsesyan <sofi170707@gmail.com>
2025-08-28 02:42:39 +0400

    game working with start btn, timer, total wins added

--- script.js
+++ script.js
@@ -1,10 +1,80 @@
 let boxContainer = document.getElementById("boxContainer")
-let boxes, box, num;
+let boxes, box, num, win, time;
+
 let giftDiv = document.createElement("div")
 giftDiv.className = "giftDiv"
+
 let giftImg = document.createElement("img")
 giftImg.src = "img/gift.png"
-giftDiv.appendChild(giftImg) 
+giftDiv.appendChild(giftImg)
+
+let btn = document.getElementById("btn")
+let pTimer = document.getElementById("timer")
+let pBest = document.getElementById("best")
+let wins = document.getElementById("wins")
+
+let min = 0
+let s = 0
+let ms = 0
+
+let bestScore = 0
+let curScore;
+
+let winsCount = 0
+
+btn.addEventListener("click", () => {
+    gameStart()
+})
+
+function gameStart() {
+    clearInterval(time)
+    boxContainer.innerHTML = ""
+    btn.innerHTML = "REFRESH"
+    win = false
+    min = 0
+    s = 0
+    ms = 0
+    timer()
+    createBoxes()
+    boxSelect()
+}
+
+function timeToMs(timeStr) {
+    let [min, time] = timeStr.split(":")
+
+    let [sec, ms] = time.split(".")
+    console.log(min, sec);
+    let toMs = parseInt(min) * 60000 + parseInt(sec) * 1000 + parseInt(ms)
+    return toMs
+}
+
+function timer() {
+    time = setInterval(() => {
+        if (win == true) {
+            clearInterval(time)
+            curScore = timeToMs(pTimer.innerHTML.replace("Time:", " "))
+            winsCount++
+            wins.innerHTML = `Total wins: ${winsCount}`
+            if (bestScore == 0 || bestScore > curScore) {
+                bestScore = curScore
+                pBest.innerHTML = `Best: ${min}:${s.toString().padStart(2, "0")}.${ms.toString().padStart(3, "0")}`
+            }
+        }
+
+        if (ms >= 100) {
+            ms = 0
+            s++
+        }
+
+        if (s >= 60) {
+            s = 0
+            min++
+        }
+        pTimer.innerHTML = `Time: ${min}:${s.toString().padStart(2, "0")}.${ms.toString().padStart(3, "0")}`
+
+        ms++
+    }, 10);
+}
 
 
 function createBoxes() {
@@ -17,16 +87,18 @@ function createBoxes() {
     for (let i = 0; i < 9; i++) {
         box = document.createElement("div")
         box.className = "box"
-        box.id =`box${i}`
+        box.id = `box${i}`
         if (i !== 0 && i % 3 == 0) {
-            index+=1
+            index += 1
         }
         document.getElementsByClassName(`boxes${index}`)[0].appendChild(box)
     }
+
+    randomBox()
 }
 
 function randomBox() {
-    num = Math.floor(Math.random()*9)
+    num = Math.floor(Math.random() * 9)
     let box = document.querySelectorAll(".box")
     box.forEach(box => {
         if (box.id == `box${num}`) {
@@ -39,16 +111,18 @@ function randomBox() {
 function boxSelect() {
     let box = document.querySelectorAll(".box")
     box.forEach(box => {
-        box.addEventListener("click", ()=>{
+        box.onclick =  () => {
             if (box.id == `box${num}`) {
                 giftDiv.style.opacity = 1
+                win = true
+                setTimeout(() => {
+                    gameStart()
+                }, 400);
             }
             box.style.background = "none"
-        })
+        }
     })
 }
 
-createBoxes()
-randomBox()
-boxSelect()
+
 
